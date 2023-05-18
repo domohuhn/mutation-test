82d053ff820a109c958c996980a68277626fac13
domohuhn <46851472+domohuhn@users.noreply.github.com>
2023-05-18 14:18:41 +0200

    Update tests

--- test/system_interactions_test.dart
+++ test/system_interactions_test.dart
@@ -8,9 +8,33 @@ import 'dart:io';
 
 void main() {
   test('Create a file', () {
-    var writer = SystemInteractions(true,true);
+    var writer = SystemInteractions(true, true);
     const path = 'build/moo/out.html';
     writer.createPathsAndWriteFile(path, 'Somedata');
     expect(File(path).existsSync(), true);
+  });
+
+  test('print to terminal - normal', () {
+    // more or less a manual test to check if output is visibile and no expection is thrown ...
+    var writer = SystemInteractions(false, false);
+    writer.write('write normal -');
+    writer.writeLine('writeLine');
+    writer.verboseWriteLine('verboseWriteLine');
+  });
+
+  test('print to terminal - quiet', () {
+    // more or less a manual test to check if output is visibile and no expection is thrown ...
+    var writer = SystemInteractions(true, true);
+    writer.write('write normal -');
+    writer.writeLine('writeLine');
+    writer.verboseWriteLine('verboseWriteLine');
+  });
+
+  test('print to terminal - verbose', () {
+    // more or less a manual test to check if output is visibile and no expection is thrown ...
+    var writer = SystemInteractions(true, false);
+    writer.write('write normal -');
+    writer.writeLine('writeLine');
+    writer.verboseWriteLine('verboseWriteLine');
   });
 }
